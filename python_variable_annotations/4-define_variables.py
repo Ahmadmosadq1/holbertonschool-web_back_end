8720bf5b849af33ca61ac3d9e85af01f07698182
Ahmadmosadq1 <eng.ahmedmosadq@gmail.com>
2025-07-28 18:52:17 +0300

    Task#4

---
+++ python_variable_annotations/4-define_variables.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""Task#4. Define variables"""
+"""defining variables with annotations"""
+a: int = 1
+pi: float = 3.14
+i_understand_annotations: bool = True
+school: str = "Holberton"
