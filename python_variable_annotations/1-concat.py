b1b65d8cfdb41e2de4d02946faef673911358def
Ahmadmosadq1 <eng.ahmedmosadq@gmail.com>
2025-07-28 18:32:33 +0300

    Task#1

---
+++ python_variable_annotations/1-concat.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""Task#1. Basic annotations - concat"""
+
+
+def concat(str1: str, str2: str) -> str:
+   """taking two string arguments and returning string"""
+   return str1+str2
