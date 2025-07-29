5b6c39acb9c960e5424bd768bd863ea326c236de
Ahmadmosadq1 <eng.ahmedmosadq@gmail.com>
2025-07-29 19:07:01 +0300

    Task#8

--- python_variable_annotations/8-make_multiplier.py
+++ python_variable_annotations/8-make_multiplier.py
@@ -1,6 +1,10 @@
 #!/usr/bin/env python3
 """Task#8. Complex types - functions"""
+from typing import Callable
 
-
-def make_multiplier(multiplier: float):
-  return multiplier * multiplier  
+def make_multiplier(multiplier: float) -> Callable[[float], float]:
+    """funcion takes multiplyers as a function"""
+    def multiply(x: float) -> float:
+        """multiplier function"""
+        return x * multiplier
+    return multiply
