f7ba3885494838dc9d72f65c390dbaa69d04fc0b
Ahmadmosadq1 <eng.ahmedmosadq@gmail.com>
2025-07-29 18:58:12 +0300

    Task#7

--- python_variable_annotations/7-to_kv.py
+++ python_variable_annotations/7-to_kv.py
@@ -3,5 +3,5 @@
 from typing import Union, Tuple
 
 
-def to_kv(k: str, v: float | int) -> Tuple[str, float]:
-    return (k, (v ** 2))
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
+    return (k, v**2)
