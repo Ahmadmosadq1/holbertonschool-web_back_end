295caa373a9aa61c4a8bfe17c59eb6d17dc1267d
Ahmadmosadq1 <eng.ahmedmosadq@gmail.com>
2025-07-29 19:11:28 +0300

    Task#8

---
+++ python_variable_annotations/9-element_length.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""Task#9. Let's duck type an iterable object"""
+from typing import Iterable, Sequence, List, Tuple
+
+
+def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
+    """adding notations on the function"""
+    return [(i, len(i)) for i in lst]
