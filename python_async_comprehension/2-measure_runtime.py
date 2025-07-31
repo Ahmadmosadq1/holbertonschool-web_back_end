a0e8be5b6dc459668e5bad3be7488b6a91d9702d
Ahmadmosadq1 <eng.ahmedmosadq@gmail.com>
2025-07-31 19:46:40 +0300

    Task#0

--- python_async_comprehension/2-measure_runtime.py
+++ python_async_comprehension/2-measure_runtime.py
@@ -1,14 +1,13 @@
 #!/usr/bin/env python3
-''' Desription: Import async_comprehension from the previous file and write 
+''' Desription: Import async_comprehension from the previous file
 '''
 from asyncio import gather
 from time import time
-
-
 async_comprehension = __import__('1-async_comprehension').async_comprehension
+
+
 async def measure_runtime() -> float:
-    ''' Measure the runtime of async_comprehension executed 4 times in
-        parallel. '''
+    ''' Measuring the runtime of async_comprehension. '''
     first_time = time()
     await gather(async_comprehension(), async_comprehension(),
                  async_comprehension(), async_comprehension())
