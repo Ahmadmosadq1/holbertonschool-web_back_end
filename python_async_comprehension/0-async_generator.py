ba75f374e8a717e16fc84cd8c95d61d4e4694136
Ahmadmosadq1 <eng.ahmedmosadq@gmail.com>
2025-07-31 19:35:06 +0300

    Task#0

---
+++ python_async_comprehension/0-async_generator.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+''' Description: Writing a coroutine called async_generator that takes no
+'''
+
+from asyncio import sleep
+from random import random
+from typing import Generator
+
+
+async def async_generator() -> Generator[float, None, None]:
+    ''' Generator that yields a random value between 0 and 10 every second,
+        10 times. '''
+    for i in range(10):
+        await sleep(1)
+        yield 10 * random()
