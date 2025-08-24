9987ec8ac5ab597f5e0ff4df968780f9922231ca
Ahmadmosadq1 <eng.ahmedmosadq@gmail.com>
2025-08-24 11:04:38 +0300

    Task#6

---
+++ ES6_classes/6-sky_high.js
@@ -0,0 +1,21 @@
+import Building from './5-building.js';
+
+export default class SkyHighBuilding extends Building {
+  constructor(sqft, floors) {
+    super(sqft);
+    if (typeof floors !== 'number') {
+      throw new TypeError('Floors must be a number');
+    }
+    this._floors = floors;
+  }
+
+  // Getter
+  get floors() {
+    return this._floors;
+  }
+
+  // Override method
+  evacuationWarningMessage() {
+    return `Evacuate slowly the ${this._floors} floors`;
+  }
+}
