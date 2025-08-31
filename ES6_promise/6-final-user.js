fb708893ab235935e7211261a12bcdd9d5e4a745
Ahmadmosadq1 <eng.ahmedmosadq@gmail.com>
2025-08-31 16:18:48 +0300

    Task#6

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -2,12 +2,13 @@ import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
-  const signUpPromise = signUpUser(firstName, lastName);
-  const uploadPromise = uploadPhoto(fileName);
-
-  return Promise.allSettled([uploadPromise, signUpPromise])
-    .then((results) => results.map((res) => ({
+  return Promise.allSettled([
+    uploadPhoto(fileName),
+    signUpUser(firstName, lastName),
+  ]).then((results) =>
+    results.map((res) => ({
       status: res.status,
       value: res.status === 'fulfilled' ? res.value : res.reason,
-    })));
+    }))
+  );
 }
