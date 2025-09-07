a6dc6c9e5b24b383e5f4901fb5b152ef8f69c160
Ahmadmosadq1 <eng.ahmedmosadq@gmail.com>
2025-09-07 13:13:03 +0300

    Task#1

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,10 +1,14 @@
+#!/usr/bin/node
 process.stdout.write("Welcome to Holberton School, what is your name?\n");
-
+process.stdin.setEncoding("utf8");
 process.stdin.on("data", (data) => {
-  const name = data.toString().trim();
+  const name = data.trim();
   process.stdout.write(`Your name is: ${name}\n`);
 });
-
 process.stdin.on("end", () => {
   process.stdout.write("This important software is now closing\n");
+});
+process.on("SIGINT", () => {
+  process.stdout.write("This important software is now closing\n");
+  process.exit(0);
 });
