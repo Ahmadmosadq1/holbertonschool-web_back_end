8fd22b9f037eccac756556b5e5f2802a7bfc836d
Ahmadmosadq1 <eng.ahmedmosadq@gmail.com>
2025-09-07 16:08:55 +0300

    Task#7

--- Node_JS_basic/7-http_express.js
+++ Node_JS_basic/7-http_express.js
@@ -16,3 +16,42 @@ function countStudents(path) {
 
       const fields = {};
       for (const row of students) {
+        const parts = row.split(',');
+        const firstname = parts[0];
+        const field = parts[3];
+        if (field) {
+          if (!fields[field]) fields[field] = [];
+          fields[field].push(firstname);
+        }
+      }
+
+      for (const [field, names] of Object.entries(fields)) {
+        output += `Number of students in ${field}: ${names.length}. List: ${names.join(', ')}\n`;
+      }
+
+      resolve(output.trim());
+    });
+  });
+}
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.type('text/plain').send('Hello Holberton School!');
+});
+
+app.get('/students', async (req, res) => {
+  res.type('text/plain');
+  const dbPath = process.argv[2];
+
+  try {
+    const summary = await countStudents(dbPath);
+    res.send(`This is the list of our students\n${summary}`);
+  } catch (e) {
+    res.send(`This is the list of our students\n${e.message}`);
+  }
+});
+
+app.listen(1245);
+
+module.exports = app;
